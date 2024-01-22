1f09fe56327347a1c8105483f889d984004e92fd
Heet Vakharia <heetkv@gmail.com>
2024-01-22 12:06:07 +0530

    Fixed type error

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,7 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT),
 });
 (async () => { 
     initDB(pool);
